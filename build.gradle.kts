4c4b9bcbf72b0b6b3886e0c74fcc937e1611e48b
Raimon Lapuente <raimon@getbalance.com>
2025-02-05 16:10:24 +0100

    transform to gradle 7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,63 +1,65 @@
 plugins {
-    id 'com.android.library'
-    id 'org.jetbrains.kotlin.android'
-    id 'maven-publish'
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
-group = 'com.github.raimonbal'
+group = "com.github.raimonbal"
 
 android {
-    namespace 'com.balance.deviceidsdk'
-    compileSdk 34
+    namespace = "com.balance.deviceidsdk"
+    compileSdk = 34
 
     defaultConfig {
-        minSdk 21
-        targetSdk 34
-        
-        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles "consumer-rules.pro"
+        minSdk = 21
+        targetSdk = 34
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
-    
+
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
-    
+
     kotlinOptions {
-        jvmTarget = '1.8'
+        jvmTarget = "1.8"
     }
 }
 
 repositories {
     google()
-    mavenCentral() // Add this if you use dependencies from Maven Central
-    maven("https://jitpack.io") // Add this if you use JitPack dependencies
+    mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    implementation 'androidx.core:core-ktx:1.12.0'
-    implementation 'androidx.appcompat:appcompat:1.6.1'
-    testImplementation 'junit:junit:4.13.2'
-    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 }
-
 
 // Publishing configuration
 afterEvaluate {
     publishing {
         publications {
-            release(MavenPublication) {
-                from components.release
-                groupId = 'com.balance.deviceidsdk'
-                artifactId = 'deviceid'
-                version = '0.0.1'
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.balance.deviceidsdk"
+                artifactId = "deviceid"
+                version = "0.0.1"
             }
         }
     }
